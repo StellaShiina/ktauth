a0f8d5973196f83b956a30822484aaa4965b5818
StellaShiina <chenclch@outlook.com>
2026-09-11 23:28:15 +0800

    fix some bugs

--- worker/build.mjs
+++ worker/build.mjs
@@ -5,6 +5,6 @@ mkdirSync("dist", { recursive: true })
 
 cpSync("site", "dist", { recursive: true })
 cpSync("../scripts/install.sh", "dist/install.sh")
-cpSync("../docker-compose.yaml", "dist/docker-compose.yaml")
+cpSync("../compose.yaml", "dist/compose.yaml")
 cpSync("../sql/00-init.sql", "dist/00-init.sql")
 cpSync("../.env.example", "dist/.env.example")
